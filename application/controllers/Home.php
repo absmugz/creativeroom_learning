6bafed6d4567e85008305c6e014cfce6053a51b1
absmugz <absmugz09@gmail.com>
2016-03-16 22:51:20 +0200

    home page

---
+++ application/controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Home extends CI_Controller {
+    
+   function __construct() {
+        parent::__construct();
+        //$this->load->model('recipes_model');
+        //$this->load->library('ion_auth');
+    }
+  
+
+    //the function that loads the first form<br><br>
+    public function index() {
+
+$data['main_content'] = 'home';
+$this->load->view('includes/template', $data);
+
+}
+
+    }
